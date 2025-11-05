99d6b1d6a700c4ae2eb56e6e75b40d4e2441529b
Haotian Zhang <zht050616@qq.com>
2025-11-05 16:20:38 +0800

    一个关于链表基本操作的文件，目前只包含链表的初始化，和遍历输出

---
+++ LinkList.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef int Datatype;
+
+/*线性表的单链表存储结构*/
+typedef struct Lnode{
+	Datatype data;
+	struct Lnode * next;
+
+}LNode, *LinkList;
+
+/*生成一个带头节点的空的单链表*/
+void Initlist(LinkList L){
+	L = (LinkList)malloc(sizeof(LNode));		/*生成一个LNode节点，并将该节点起始地址赋值给L*/
+	L->next = NULL;					/*将该节点的指针域置为空*/
+}
+
+/*建立有n个节点的单链表(倒序输入数据)*/
+void Create_L(LinkList  L,int n){
+	Initlist(L);					/*生成一个带头节点的空单链表*/
+	LinkList p;
+	for(int i = n;i > 0;i--){
+		p = (LinkList)malloc(sizeof(LNode));	/*生成一个LNode节点，将该节点的初始地址赋值给p*/
+		printf("请输入第%d个数据",i);
+		scanf("%d",&(p->data));			/*将数据输入数据域*/
+		p->next = L->next;			/*将上一个生成的节点连接到新的节点的后面*/
+		L->next = p;				/*将新建的节点连接到头节点的后面*/
+	}
+
+
+}
+
+
+/*输出链表的全部内容*/
+void output(LinkList  L){
+	LinkList p = L->next;				/*将p指向首元节点*/
+	while(1){
+		printf("%d\n",p->data);			/*输出当前节点的数据域*/			
+		p = p->next;				/*将指针指p向下一个节点*/
+		if(!(p)){				/*如果下一个节点是空的，就退出*/
+			break;
+		}
+	}
+}
+
+
+int main(){
+	LinkList L;
+	Create_L(L,5);
+	output(L);
+	return 0;
+}
